325674c2b9042f5cbb5bfd6dff3e3ab13d063dcd
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-04-11 03:11:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
    get '/:operation/:number1/:number2' do 
       @num_1=params[:number1].to_i
       @num_2=params[:number2].to_i
-      @additon=@num_1+@num_2-
+      @additon=@num_1+@num_2
       @subtraction=@num_2-@num_1
       @multiple=@num_1*@num_2
       @divide=@num_1/@num_2
