07346787b5f2cf1f73a28660b0d8ec1e6a097379
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-04-11 03:14:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
    get '/:operation/:number1/:number2' do 
       @num_1=params[:number1].to_i
       @num_2=params[:number2].to_i
-       "#{@num_1+@num_2}"
+       "#{@num_1 + @num_2}"
        "#{ @num_2 - @num_1}"
        "#{@num_1* @num_2}"
        "#{ @num_1/@num_2}"
